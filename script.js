443b7b6a8e25e943b19be161898e391d176b1f36
ETKorea <162276298+ETKorea@users.noreply.github.com>
2024-05-06 11:30:09 +0900

    temporarily

--- script.js
+++ script.js
@@ -1,52 +1,70 @@
-const imageUpload = document.getElementById('imageUpload') // 여전히 사진이 업로드가되지않아 이유를 파악하는게 우선일 듯
+const imageUpload = document.getElementById('imageUpload'); // 여전히 사진이 업로드가되지않아 이유를 파악하는게 우선일 듯
 
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start)
+]).then(start);
 
 async function start() {
-  const container = document.createElement('div')
-  container.style.position = 'relative'
-  document.body.append(container)
-  const labeledFaceDescriptors = await loadLabeledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6) //60퍼센트 이상 얼굴 인식될경우 얼굴로 판단
-  let image
-  let canvas
-  document.body.append('Loaded')
+  const container = document.createElement('div');
+  container.style.position = 'relative';
+  document.body.append(container);
+  const labeledFaceDescriptors = await loadLabeledImages();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+  let image;
+  let canvas;
+  document.body.append('Loaded');
   imageUpload.addEventListener('change', async () => {
-    if (image) image.remove()
-    if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(imageUpload.files[0])
-    container.append(image)
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-    const displaySize = { width: image.width, height: image.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    if (image) image.remove();
+    if (canvas) canvas.remove();
+    image = await faceapi.bufferToImage(imageUpload.files[0]);
+    container.append(image);
+    canvas = faceapi.createCanvasFromMedia(image);
+    container.append(canvas);
+    const displaySize = { width: image.width, height: image.height };
+    faceapi.matchDimensions(canvas, displaySize);
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
     results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
       drawBox.draw(canvas)
-    })
-  })
+    });
+  });
 }
 
-function loadLabeledImages() {
-  const labels = ['Choi Minsik', 'Kim Goeun', 'Lee Dohyun', 'Yoo Haejin']
+async function loadLabeledImages() {
+  const labels = ['Choi Minsik', 'Kim Goeun', 'Lee Dohyun', 'Yoo Haejin'];
+  const labeledFaceDescriptors = [];
+  
+  for (const label of labels) {
+    const descriptions = [];
+    
+    for (let i = 1; i <= 9; i++) {
+      const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ETKorea/graphicsmisson/main/labeled_images/${label}/${i}.jpg`);
+      const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+      descriptions.push(detections.descriptor);
+    }
+    
+    labeledFaceDescriptors.push(new faceapi.LabeledFaceDescriptors(label, descriptions));
+  }
+  
+  return labeledFaceDescriptors;
+}
+/*async function loadLabeledImages() {
+  const labels = ['Choi Minsik', 'Kim Goeun', 'Lee Dohyun', 'Yoo Haejin'];
   return Promise.all(
     labels.map(async label => {
       const descriptions = []//https://github.com/WebDevSimplified/Face-Recognition-JavaScript/tree/master/labeled_images
       for (let i = 1; i <= 9; i++) {//tree를 blob로 바꿔보기 or 전체 코드끝에 ; 붙이기 
-        const img = await faceapi.fetchImage(`https://github.com/ETKorea/graphicsmisson/tree/main/labeled_images/${label}/${i}.jpg`)
-        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-        descriptions.push(detections.descriptor)
+        const img = await faceapi.fetchImage(`https://github.com/ETKorea/graphicsmisson/tree/main/labeled_images/${label}/${i}.jpg`);
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+        descriptions.push(detections.descriptor);
       }
 
-      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
     })
   )
-}
+}*/
