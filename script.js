39630d0d0b1b9d21a6a2175e63cf80c1ad423e15
ETKorea <162276298+ETKorea@users.noreply.github.com>
2024-05-07 14:33:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
-const imageUpload = document.getElementById('imageUpload'); // 여전히 사진이 업로드가되지않아 이유를 파악하는게 우선일 듯
+const imageUpload = document.getElementById('imageUpload') // 여전히 사진이 업로드가되지않아 이유를 파악하는게 우선일 듯
 
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start);
+]).then(start)
 
 async function start() {
-  const container = document.createElement('div');
-  container.style.position = 'relative';
-  document.body.append(container);
-  const labeledFaceDescriptors = await loadLabeledImages();
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
-  let image;
-  let canvas;
-  document.body.append('Loaded');
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  let image
+  let canvas
+  document.body.append('Loaded')
   imageUpload.addEventListener('change', async () => {
-    if (image) image.remove();
-    if (canvas) canvas.remove();
+    if (image) image.remove()
+    if (canvas) canvas.remove()
     image = await faceapi.bufferToImage(imageUpload.files[0]);
-    container.append(image);
+    container.append(image)
     canvas = faceapi.createCanvasFromMedia(image);
-    container.append(canvas);
-    const displaySize = { width: image.width, height: image.height };
-    faceapi.matchDimensions(canvas, displaySize);
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
     results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
       drawBox.draw(canvas)
-    });
-  });
+    })
+  })
 }
 
 async function loadLabeledImages() {
-  const labels = ['Choi Minsik', 'Kim Goeun', 'Lee Dohyun', 'Yoo Haejin'];
+  const labels = ['Choi Minsik', 'Kim Goeun', 'Lee Dohyun', 'Yoo Haejin']
   return Promise.all(
     labels.map(async label => {
-      const descriptions = [];
+      const descriptions = []
       for (let i = 1; i <= 9; i++) {
-        const img = await faceapi.fetchImage('https://raw.githubusercontent.com/ETKorea/graphicsmisson/main/labeled_images/${label}/${i}.jpg');
-        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-        descriptions.push(detections.descriptor);
+        const img = await faceapi.fetchImage('https://raw.githubusercontent.com/ETKorea/graphicsmisson/main/labeled_images/${label}/${i}.jpg')
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
       }
 
-      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
     })
   )
 }
 /*async function loadLabeledImages() {
-  const labels = ['Choi Minsik', 'Kim Goeun', 'Lee Dohyun', 'Yoo Haejin'];
-  const labeledFaceDescriptors = [];
+  const labels = ['Choi Minsik', 'Kim Goeun', 'Lee Dohyun', 'Yoo Haejin']
+  const labeledFaceDescriptors = []
   
   for (const label of labels) {
-    const descriptions = [];
+    const descriptions = []
     
     for (let i = 1; i <= 9; i++) {
-      const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ETKorea/graphicsmisson/main/labeled_images/${label}/${i}.jpg`);
-      const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-      descriptions.push(detections.descriptor);
+      const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ETKorea/graphicsmisson/main/labeled_images/${label}/${i}.jpg`)
+      const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+      descriptions.push(detections.descriptor)
     }
     
-    labeledFaceDescriptors.push(new faceapi.LabeledFaceDescriptors(label, descriptions));
+    labeledFaceDescriptors.push(new faceapi.LabeledFaceDescriptors(label, descriptions))
   }
   
-  return labeledFaceDescriptors;
+  return labeledFaceDescriptors
 }*/
